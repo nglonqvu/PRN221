eaa14b7b463abdf51c336e94013b949407d60185
Pham Van Manh <121953255+ManhMau@users.noreply.github.com>
2024-02-23 23:27:01 +0700

    UI Shopping Cart,, order detail

---
+++ Web_PizzaShop/Pages/Customer/Ordering.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Web_PizzaShop.Pages.Customer
+{
+    public class OrderingModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
